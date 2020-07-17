875ff5b874a7ab78d9d406404f7ecfe009682c3e
Pawar <shrikant.pawar@yale.edu>
2020-07-17 19:29:12 -0400

    Add files via upload

---
+++ Pulmonary-Embolism-Master.py
@@ -0,0 +1,136 @@
+#Pulmonary Embolism Prediction, Shrikant Pawar
+
+#Variable Descriptions
+Subject (PTID) Participant ID
+RID Participant roster ID
+Image.Data.ID MRI ID
+Modality Image type
+Visit 1=screening scan
+Acq.Date MRI date
+DX.bl Diagnosis at baseline
+EXAMDATE Examination Date
+AGE Age at baseline
+PTGENDER Sex
+PTEDUCAT Years of Education
+PTETHCAT Ethnicity
+PTRACCAT Race
+Wells Score
+
+import numpy as np
+import sklearn as sk
+import pandas as pd
+import os
+
+#Read data
+dat = pd.read_csv('.csv')
+
+#Exploratory Data Analysis (EDA)
+
+numerical_vars = ['AGE', 'Wells', 'PTEDUCAT']
+cat_vars = list(set(features) - set(numerical_vars))
+
+print('Categorical variable distributions:\n')
+
+for var in cat_vars:
+    print('\nDistribution of', var)
+    
+    print(X[var].value_counts())
+
+
+
+
+Numerical Variable Distributions
+All 3 distributions seem a bit right-skewed
+Age: mean = 75, min=55, max=89 - seems to be no obvious bad data
+Wells: mean = 26.9, min=20, max=30 - seems to be no obvious bad data, NOTE: Wells $\in ~(0,30)$
+PTEDUCAT: mean = 15.6, min=6, max=20 - seems to be no obvious bad data. 13-14 yrs is probably high school/GED and 17-18 years is likely Bachelor's degree
+
+
+from matplotlib import pyplot as plt
+%matplotlib inline
+
+print('Numerical Var Distributions:\n')
+
+for var in numerical_vars:
+    plt.hist(X[var], bins=10)
+    plt.title(var + ' Distribution')
+    plt.show()
+    
+    # descriptive stats
+    print(X[var].describe())
+
+
+#for each categorical var, convert to 1-hot encoding
+for var in cat_vars:
+    print('Converting', var, 'to 1-hot encoding')
+    
+    #get 1-hot and replace original column with the >= 2 categories as columns
+    one_hot_df = pd.get_dummies(X[var])
+    X = pd.concat([X, one_hot_df], axis=1)
+    X = X.drop(var, axis=1)
+    
+X.head(4)
+
+#Train/Test Split
+
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=.2)
+
+print('X_train:', X_train.shape, '\ty_train:', y_train.shape)
+print('X_test:', X_test.shape, '\ty_test:', y_test.shape)
+num_test = X_test.shape[0]
+
+
+Logistic Regression, Random Forest, KNN, ML
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.neural_network import MLPClassifier
+
+from sklearn.model_selection import cross_val_score
+
+log_clf = LogisticRegression(solver='lbfgs', penalty='l2', max_iter=1000000, multi_class='multinomial')
+print('Validation Accuracy = ', format(cross_val_score(log_clf, X_train, y_train, cv=5).mean(), '.2%'))
+
+rf_clf = RandomForestClassifier(n_estimators=200)
+print('Validation Accuracy = ', format(cross_val_score(rf_clf, X_train, y_train, cv=5).mean(), '.2%'))
+
+knn_clf = KNeighborsClassifier(n_neighbors=10)
+print('Validation Accuracy = ', format(cross_val_score(knn_clf, X_train, y_train, cv=5).mean(), '.2%'))
+
+mlp_clf = MLPClassifier(hidden_layer_sizes=(15, 10), alpha=3, learning_rate='adaptive', max_iter=100000)
+print('Validation Accuracy = ', format(cross_val_score(mlp_clf, X_train, y_train, cv=5).mean(), '.2%'))
+
+# Choosing Logistic Regression based on above accuracies
+
+# Test Logistic Regression Model
+
+log_clf.fit(X_train, y_train)
+
+log_clf_preds = log_clf.predict(X_test)
+    
+log_clf_accuracy = (log_clf_preds == y_test)
+print('Test Accuracy =', format( np.sum(log_clf_accuracy)/num_test, '.2%'))
+
+# Analyze misclassifications
+
+misclassified_index = (log_clf_preds != y_test)
+
+X_misclassified = X_test[misclassified_index]
+y_misclassified = y_test[misclassified_index]
+preds_misclassified = log_clf_preds[misclassified_index]
+
+X_misclassified.head()
+
+
+
+
+
+
+
+
+
+
+    
